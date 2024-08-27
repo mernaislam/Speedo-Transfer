2adb36877341fbba870efb24d69f0ab3b4696cbf
Malak Mohamed <132229834+MalakMohameed@users.noreply.github.com>
2024-08-27 12:09:56 -0700

    Framing my Environment
    Just made my groups and structured my workspace

---
+++ Registration/Registration/Controllers/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Registration
+//
+//  Created by Malak Mohamed on 27/08/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
