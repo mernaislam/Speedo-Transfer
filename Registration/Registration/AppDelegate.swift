6d41393afc190c743ce8bbb4b5883cafb49737d2
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2024-09-04 00:29:53 +0300

    feat: Add Animated Tab Controller

--- Registration/Registration/AppDelegate.swift
+++ Registration/Registration/AppDelegate.swift
@@ -13,42 +13,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let isFirstOpen = UserDefaultsManager.shared().isFirstOpen
-        
-        if isFirstOpen {
-            showOnBoardingScreen()
-        } else {
-            switchToRegisterScreen()
-        }
-        
+//        let isFirstOpen = UserDefaultsManager.shared().isFirstOpen
+//        
+//        if isFirstOpen {
+//            showOnBoardingScreen()
+//        } else {
+//            switchToRegisterScreen()
+//        }
+        switchToHomeScreen()
         return true
 
     }
     
     func showOnBoardingScreen(){
         let storyboard = UIStoryboard(name: Storyboard.OnBoardingScreen, bundle: nil)
-
         let initialViewController = storyboard.instantiateViewController(withIdentifier: ViewControllersID.OnBoarding)
-
         self.window?.rootViewController = initialViewController
-        setBackgroundColorWhiteToRed()
-    }
-    
-    func setBackgroundColorWhiteToRed(){
-        if let rootView = self.window?.rootViewController?.view {
-            let backgroundLayer = AppColors.gradientLayer
-            backgroundLayer.frame = rootView.bounds
-            rootView.layer.insertSublayer(backgroundLayer, at: 0)
-        }
     }
     
     func switchToRegisterScreen(){
         let storyboard = UIStoryboard(name: Storyboard.Main, bundle: nil)
-
         let initialViewController = storyboard.instantiateViewController(withIdentifier: ViewControllersID.Register)
-
         self.window?.rootViewController = initialViewController
-        setBackgroundColorWhiteToRed()
+    }
+       
+    func switchToHomeScreen() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let initialViewController = AnimatedTabBarController()
+        self.window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
     }
 }
 
