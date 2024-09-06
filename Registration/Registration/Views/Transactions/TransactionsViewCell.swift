24b43470a5006de46989ee9c66e2aa88b9367e8a
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2024-09-06 20:57:49 +0300

    feat: Add UI for transaction screen

---
+++ Registration/Registration/Views/Transactions/TransactionsViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  TransactionsViewCell.swift
+//  Registration
+//
+//  Created by Merna Islam on 04/09/2024.
+//
+
+import UIKit
+
+class TransactionsViewCell: UITableViewCell {
+    
+    // MARK: - Static Properties
+    static let identifier = "TransactionsViewCell"
+    static let nib = UINib(nibName: "TransactionsViewCell", bundle: nil)
+    
+    // MARK: - IBOutlet
+    @IBOutlet var cellView: UIView!
+    
+    // MARK: - Lifecycle Methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    // MARK: - Other Methods
+    func configureCell(){
+        
+    }
+    
+}
