bda8c66d18003efb934cb176c4c225c7d45f4bcc
Malak Mohamed <132229834+MalakMohameed@users.noreply.github.com>
2024-09-11 16:30:29 -0700

    feat: Token Expired Screen
    logic and design implemented

--- Registration/Registration/Manager/APIManager.swift
+++ Registration/Registration/Manager/APIManager.swift
@@ -4,20 +4,32 @@
 //
 //  Created by Malak Mohamed on 10/09/2024.
 //
-
-import Foundation
+import UIKit
 import Alamofire
 
 class APIManager {
-    
+
     private static let baseURL = "https://money-transfer-production.up.railway.app/api"
+    
+    // Handle token expiration and present TimeOutVC
+    private static func handleTokenExpiration() {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            if let rootViewController = windowScene.windows.first?.rootViewController {
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                if let timeOutVC = storyboard.instantiateViewController(withIdentifier: "TimeOutVC") as? TimeOutVC {
+                    rootViewController.present(timeOutVC, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+
     
     static func registerUser(user: User, completion: @escaping (Result<Any, Error>) -> Void) {
         let url = URL(string: "\(baseURL)/register")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-
+        
         do {
             let jsonData = try JSONEncoder().encode(user)
             request.httpBody = jsonData
@@ -25,19 +37,19 @@ class APIManager {
             completion(.failure(error))
             return
         }
-
+        
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
                 completion(.failure(error))
                 return
             }
-
+            
             guard let data = data else {
                 let noDataError = NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data returned"])
                 completion(.failure(noDataError))
                 return
             }
-
+            
             // Debugging: Print the response data
             if let responseString = String(data: data, encoding: .utf8) {
                 print("Response Data: \(responseString)")
@@ -46,8 +58,7 @@ class APIManager {
         }
         task.resume()
     }
-    
-    
+
     static func loginUser(email: String, password: String, completion: @escaping (Result<[String: Any], Error>) -> Void) {
         guard let url = URL(string: "\(baseURL)/login") else { return }
         var request = URLRequest(url: url)
@@ -85,9 +96,7 @@ class APIManager {
                     if let token = jsonResponse["token"] as? String {
                         // Save the token to the Keychain
                         TokenManager.shared.setToken(token)
-                        
                         completion(.success(jsonResponse))
-                    
                     } else {
                         // Token not found in the response, return an error
                         completion(.failure(NSError(domain: "Token not found in response", code: -1, userInfo: nil)))
@@ -98,37 +107,37 @@ class APIManager {
             }
         }.resume()
     }
+    
     static func logoutUser(completion: @escaping (Result<String, Error>) -> Void) {
-            let url = "\(baseURL)/logout"
-            
-            guard let token = TokenManager.shared.getToken() else {
-                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Token not available"])))
-                return
-            }
-            
-            let headers: HTTPHeaders = [
-                .authorization(bearerToken: token)
-            ]
-            
-            AF.request(url, method: .post, headers: headers)
-                .validate(statusCode: 200...299)
-                .responseString { response in
-                    switch response.result {
-                    case .success(let message):
-                        print("Response Data: \(message)")
-                        completion(.success(message))
-                    case .failure(let error):
-                        print("Logout request error: \(error.localizedDescription)")
-                        if let data = response.data, let errorMessage = String(data: data, encoding: .utf8) {
-                            print("Response Data: \(errorMessage)")
-                            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: errorMessage])))
-                        } else {
-                            completion(.failure(error))
-                        }
+        let url = "\(baseURL)/logout"
+        
+        guard let token = TokenManager.shared.getToken() else {
+            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Token not available"])))
+            return
+        }
+        
+        let headers: HTTPHeaders = [
+            .authorization(bearerToken: token)
+        ]
+        
+        AF.request(url, method: .post, headers: headers)
+            .validate(statusCode: 200...299)
+            .responseString { response in
+                switch response.result {
+                case .success(let message):
+                    print("Response Data: \(message)")
+                    completion(.success(message))
+                case .failure(let error):
+                    print("Logout request error: \(error.localizedDescription)")
+                    if let data = response.data, let errorMessage = String(data: data, encoding: .utf8) {
+                        print("Response Data: \(errorMessage)")
+                        completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: errorMessage])))
+                    } else {
+                        completion(.failure(error))
                     }
                 }
-        }
-
+            }
+    }
     
     static func getBalance(completion: @escaping (Result<Double, Error>) -> Void) {
         let url = "\(self.baseURL)/balance"
@@ -141,28 +150,32 @@ class APIManager {
         ]
         
         AF.request(url, method: .get, headers: headers)
-               .validate()
-               .responseData { response in
-                   switch response.result {
-                   case .success(let data):
-                       do {
-                           if let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-                              let balance = jsonResponse["balance"] as? Double {
-                               completion(.success(balance))
-                           } else {
-                               completion(.failure(NSError(domain: "Invalid response format", code: -1, userInfo: nil)))
-                           }
-                       } catch {
-                           completion(.failure(error))
-                       }
-                       
-                   case .failure(let error):
-                       completion(.failure(error))
-                   }
-               }
+            .validate()
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    do {
+                        if let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                           let balance = jsonResponse["balance"] as? Double {
+                            completion(.success(balance))
+                        } else {
+                            completion(.failure(NSError(domain: "Invalid response format", code: -1, userInfo: nil)))
+                        }
+                    } catch {
+                        completion(.failure(error))
+                    }
+                    
+                case .failure(let error):
+                    // Check if the error is related to authentication
+                    if let afError = error.asAFError, afError.responseCode == 401 {
+                        handleTokenExpiration()
+                    }
+                    completion(.failure(error))
+                }
+            }
     }
     
-    static func transfer(to accNum: String, amount: Double, completion: @escaping (Result<Data, Error>) -> Void){
+    static func transfer(to accNum: String, amount: Double, completion: @escaping (Result<Data, Error>) -> Void) {
         let url = "\(self.baseURL)/transfer"
         guard let token = TokenManager.shared.getToken() else { return }
         
@@ -171,19 +184,23 @@ class APIManager {
             .accept("application/json")
         ]
         
-        AF.request(url, method: .get, headers: headers)
-               .validate()
-               .responseData { response in
-                   switch response.result {
-                   case .success(let data):
-                       completion(.success(data))
-                   case .failure(let error):
-                       completion(.failure(error))
-                   }
-               }
+        AF.request(url, method: .post, parameters: ["account": accNum, "amount": amount], headers: headers)
+            .validate()
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    completion(.success(data))
+                case .failure(let error):
+                    // Check if the error is related to authentication
+                    if let afError = error.asAFError, afError.responseCode == 401 {
+                        handleTokenExpiration()
+                    }
+                    completion(.failure(error))
+                }
+            }
     }
     
-    static func getTransactions(completion: @escaping (Result<[TransactionModel], Error>) -> Void){
+    static func getTransactions(completion: @escaping (Result<[TransactionModel], Error>) -> Void) {
         let url = "\(self.baseURL)/transactions"
                 
         guard let token = TokenManager.shared.getToken() else {
@@ -202,9 +219,12 @@ class APIManager {
             case .success(let transactions):
                 completion(.success(transactions))
             case .failure(let error):
+                // Check if the error is related to authentication
+                if let afError = error.asAFError, afError.responseCode == 401 {
+                    handleTokenExpiration()
+                }
                 completion(.failure(error))
             }
         }
     }
-
 }
