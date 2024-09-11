feb8379ca9484a1eb2f3ed1c41338950f675bead
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2024-09-11 03:42:09 +0300

    feat: Use get balance API in home

--- Registration/Registration/Controllers/HomeVC.swift
+++ Registration/Registration/Controllers/HomeVC.swift
@@ -16,20 +16,64 @@ class HomeVC: UIViewController {
     
     // MARK: - Properties
     var tabSwitchDelegate: TabSwitchProtocol!
+    private let activityIndicator = UIActivityIndicatorView(style: .large)
     
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         self.initiateVC()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.activityIndicator.startAnimating()
+        self.getBalance()
+    }
 
     // MARK: - Private Methods
-    private func initiateVC(){
+    private func initiateVC() {
         self.applyGradientBgYellowToRed()
         self.setupNavigationBorder()
         self.profileView.cornerRadius = profileView.frame.width / 2
         self.setupTableView()
+        self.setupActivityIndicator()
+        self.toggleViewsVisibility(alpa: 0)
     }
+    
+    private func setupActivityIndicator(){
+        self.activityIndicator.center = self.view.center
+        self.activityIndicator.hidesWhenStopped = true
+        self.activityIndicator.color = .gray
+        self.view.addSubview(activityIndicator)
+    }
+    
+    private func toggleViewsVisibility(alpa: Double){
+        self.profileView.alpha = alpa
+        self.balanceLabel.alpha = alpa
+        self.transactionTableView.alpha = alpa
+    }
+    
+    private func getBalance() {
+        APIManager.getBalance { result in
+            DispatchQueue.main.async {
+                self.activityIndicator.stopAnimating()
+                UIView.animate(withDuration: 0.3) {
+                    self.toggleViewsVisibility(alpa: 1)
+                }
+        
+                switch result {
+                case .success(let balance):
+                    let balanceInt = Int(balance)
+                    self.balanceLabel.text = "\(balanceInt)"
+                    print("Balance: \(balanceInt)")
+                    
+                case .failure(let error):
+                    self.balanceLabel.text = "Error"
+                    print("Error fetching balance: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+
     
     private func setupTableView(){
         transactionTableView.register(HomeTransactionCell.nib, forCellReuseIdentifier: HomeTransactionCell.identifier)
