98a35c16f937c1fa7fb8bfdb06b86cd2dc78dee8
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2024-09-11 03:40:49 +0300

    feat: create keychain for tokens

---
+++ Registration/Registration/Manager/KeychainManager.swift
@@ -0,0 +1,49 @@
+//
+//  KeychainManager.swift
+//  Registration
+//
+//  Created by Merna Islam on 11/09/2024.
+//
+
+import Security
+import Foundation
+
+class KeychainManager {
+    
+    static func saveTokenToKeychain(token: String) {
+        let tokenData = token.data(using: .utf8)!
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: "userToken",
+            kSecValueData as String: tokenData,
+        ]
+        
+        SecItemDelete(query as CFDictionary)
+        
+        SecItemAdd(query as CFDictionary, nil)
+        
+    }
+    
+    static func getTokenFromKeychain() -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: "userToken",
+            kSecReturnData as String: kCFBooleanTrue!,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        
+        var dataTypeRef: AnyObject? = nil
+        
+        let status = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+        
+        if status == errSecSuccess {
+            if let retrievedData = dataTypeRef as? Data {
+                return String(data: retrievedData, encoding: .utf8)
+            }
+        }
+        
+        return nil
+    }
+    
+}
---
+++ Registration/Registration/Manager/TokenManager.swift
@@ -0,0 +1,26 @@
+//
+//  TokenManager.swift
+//  Registration
+//
+//  Created by Merna Islam on 11/09/2024.
+//
+
+class TokenManager {
+    static let shared = TokenManager()
+    private init() {}
+    
+    private var token: String?
+    
+    func setToken(_ newToken: String) {
+        token = newToken
+        KeychainManager.saveTokenToKeychain(token: newToken)
+    }
+    
+    func getToken() -> String? {
+        if token == nil {
+            token = KeychainManager.getTokenFromKeychain()
+        }
+        return token
+    }
+}
+
