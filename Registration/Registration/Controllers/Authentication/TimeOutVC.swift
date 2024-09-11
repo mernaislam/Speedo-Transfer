7a97810227dc2867250630457f07cf25931045c3
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2024-09-11 17:38:50 +0300

    feat: Add timeout screen

---
+++ Registration/Registration/Controllers/Authentication/TimeOutVC.swift
@@ -0,0 +1,29 @@
+//
+//  TimeOutVC.swift
+//  Registration
+//
+//  Created by Merna Islam on 11/09/2024.
+//
+
+import UIKit
+
+class TimeOutVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
